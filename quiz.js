61c0eddd2d2530c24dda2c961c61c4c396e52eee
Temirlan <temirlan.kystaubayev@gmail.com>
2024-01-15 17:03:02 +0600

    feat: add working request to the API
    feat: add sending msg with flag picture and an inline keyboard

--- quiz.js
+++ quiz.js
@@ -3,25 +3,22 @@ const { InlineKeyboard, InputMediaBuilder, InputFile } = require("grammy");
 const axios = require("axios");
 
 function quizStart(ctx) {
-    console.log("<<< Quiz started. >>>");
+    sendQuestion(ctx);
+}
 
-    const quizInlineKeyboard = new InlineKeyboard()
+function sendQuestion(ctx) {
+    const quizOptions = new InlineKeyboard()
         .text("1")
         .text("2")
         .text("3")
         .text("4");
 
-    axios.get("https://restcountries.com/v3.1/name/kazakhstan").then((res) => res.json())
-        .then((json) => {
-            console.log(json.data);
-            const question = InputMediaBuilder.photo(new InputFile(json.data.flags.png), 
-            { caption: "Which country's flag is shown above?" });
+    axios.get("https://restcountries.com/v3.1/name/kazakhstan").then((res) => {
+            let data = res.data[0];
+            const question = InputMediaBuilder.photo(data.flags.png);
             ctx.replyWithMediaGroup([question]);
+            ctx.reply("Which country's flag is shown above?", { reply_markup: quizOptions });
     });
-
-    // const question = InputMediaBuilder.photo(new InputFile(result.flags.png), 
-    //     { caption: "Which country's flag is shown above?" });
-    // ctx.replyWithMediaGroup([photo]);
 }
 
 module.exports = { quizStart };
