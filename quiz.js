349ea77c33ac43ce41c68b16e7bd53d0da8fe8c0
Temirlan <temirlan.kystaubayev@gmail.com>
2024-01-16 15:39:41 +0600

    feat: add func that creates an array of incorrect options
    feat: add countries names in inline keyboard in question

--- quiz.js
+++ quiz.js
@@ -2,27 +2,58 @@ const { InlineKeyboard, InputMediaBuilder, InputFile } = require("grammy");
 // const RestCountriesApi = require("./rest-countries-api.js");
 const axios = require("axios");
 
+/* Main logic of the quiz.
+   Starts after pressing the "Start the Quiz" inline btn */
 async function quizStart(ctx) {
     let countriesData;
+
     await axios.get("https://restcountries.com/v3.1/all").then((res) => {
         countriesData = res.data;
     });
+
     let pickedCountry = countriesData[getRandomNum(countriesData.length)];
-    sendQuestion(ctx, pickedCountry);
+    let options = getQuestionOptions(countriesData, pickedCountry.name.common, 4)
+    sendQuestion(ctx, pickedCountry.flags.png, options);
 }
 
-async function sendQuestion(ctx, data) {
-    const quizOptions = new InlineKeyboard()
-        .text("1")
-        .text("2")
-        .text("3")
-        .text("4");
+/* Sends flag image and a question with options */
+async function sendQuestion(ctx, flag, options) {
+    const optionsRow = options.map(([label, data]) => InlineKeyboard.text(label, data));
+    const optionsKeyboard = InlineKeyboard.from([optionsRow]);
 
-    const question = InputMediaBuilder.photo(data.flags.png);
+    const question = InputMediaBuilder.photo(flag);
     await ctx.replyWithMediaGroup([question]);
-    ctx.reply("Which country's flag is shown above?", { reply_markup: quizOptions });
+    ctx.reply("Which country's flag is shown above?", { reply_markup: optionsKeyboard });
 }
 
+/* Creates an array of options */
+function getQuestionOptions(countriesData, rightCountryName, optionsNum) {
+
+    // Randomly shuffles an array element's order
+    const shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = getRandomNum(i + 1);
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    let options = [[rightCountryName, "true"]];
+
+    // Fills an array with incorrect options
+    for (let i = 0; options.length < optionsNum; i++) {
+        let falseOption = countriesData[getRandomNum(countriesData.length)].name.common;
+        // Adds only unique countries as options
+        if (!options.includes(falseOption)) options.push([falseOption, "false"]);
+    }
+
+    options = shuffle(options);
+    console.log(options);
+
+    return options;
+}
+
+/* Returns random number with maximum value as a parameter */
 function getRandomNum(max) {
     return Math.floor(Math.random() * max);
 }
